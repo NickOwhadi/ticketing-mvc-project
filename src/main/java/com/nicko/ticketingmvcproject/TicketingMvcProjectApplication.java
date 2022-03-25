eb3d4baa8e81a51087e0e73a11cda485942e147b
n7owhad <n7owhad@hotmail.com>
2022-03-25 04:42:50 -0400

    Creating Base of the project as Master, Main Template

---
+++ src/main/java/com/nicko/ticketingmvcproject/TicketingMvcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nicko.ticketingmvcproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingMvcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingMvcProjectApplication.class, args);
+    }
+
+}
