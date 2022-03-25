473aeae18fc7d23a5d0ef5e28d3c235b3d0ea868
n7owhad <n7owhad@hotmail.com>
2022-03-25 06:05:17 -0400

    Part1 Branch, Building Pojos

---
+++ src/main/java/com/nicko/ticketingmvcproject/entity/Role.java
@@ -0,0 +1,13 @@
+package com.nicko.ticketingmvcproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Role {
+    private Long id;
+    private String description;
+}
---
+++ src/main/java/com/nicko/ticketingmvcproject/entity/User.java
@@ -0,0 +1,17 @@
+package com.nicko.ticketingmvcproject.entity;
+
+import com.nicko.ticketingmvcproject.enums.Gender;
+
+
+
+public class User extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String passWord;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+}
