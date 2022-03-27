0e8d2e70a1858fa0380b6d7488a1916ba8aa2412
n7owhad <n7owhad@hotmail.com>
2022-03-27 00:48:14 -0400

    Part2 Branch, creating map to reetrive objects and return them. Implementation

---
+++ src/main/java/com/nicko/ticketingmvcproject/service/impl/RoleServiceImp.java
@@ -0,0 +1,30 @@
+package com.nicko.ticketingmvcproject.service.impl;
+
+import com.nicko.ticketingmvcproject.dto.RoleDTO;
+import com.nicko.ticketingmvcproject.service.RoleService;
+
+import java.util.List;
+
+public class RoleServiceImp extends AbstractMapService<RoleDTO, Long> implements RoleService {
+
+
+    @Override
+    public RoleDTO save(RoleDTO object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public RoleDTO findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public List<RoleDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+            super.deleteById(id);
+    }
+}
